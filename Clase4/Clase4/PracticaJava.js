b47a00f4881f818736b04508e4df0ef9f3a79b32
Bryan Gonzalez <bryang3001@hotmail.com>
2017-02-01 19:10:41 -0600

    Segundo commit

---
+++ Clase4/Clase4/PracticaJava.js
@@ -0,0 +1,16 @@
+﻿function Area_Cuadrado(num1) {
+    document.getElementById("Cuadrado1").innerHTML = "El area del cuadrado es: " + parseInt(Math.pow(num1, 2));
+}
+
+function Area_Rectangulo(num1, num2) {
+    document.getElementById("Rectangulo1").innerHTML = "El area del Rectangulo es: " + parseInt(num1) * parseInt(num2);
+}
+
+function Area_Triangulo(num1, num2) {
+    document.getElementById("Triangulo1").innerHTML = "El area del Triangulo es: " + (parseInt(num1) * parseInt(num2)) / 2;
+}
+
+
+function Area_Rombo(num1, num2) {
+    document.getElementById("Rombo1").innerHTML = "El area del Rombo es: " + (parseInt(num1) * parseInt(num2)) / 2;
+}
